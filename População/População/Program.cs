75fcc837ee05862d28d107ce2ff99bc9ee088b4e
Daniel42318 <daniel240509martins@gmail.com>
2026-03-16 18:05:48 -0300

    Add files via upload

--- População/População/Program.cs
+++ População/População/Program.cs
@@ -1,21 +1,35 @@
 ﻿using População;
 
-Professor prof = new Professor();
-Console.WriteLine("Insira o nome");
-prof.nome = Console.ReadLine();
-Console.WriteLine("Insira a idade");
-prof.idade = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Insira o CPF");
-prof.cpf = Console.ReadLine();
+Console.WriteLine("Você é aluno ou professor?");
+String escolaridade = Console.ReadLine();
 
-prof.listarDados(prof.nome, prof.idade, prof.cpf);
+PessoaFisica pessoa = new PessoaFisica();
+Console.WriteLine("Insira o nome: ");
+pessoa.nome = Console.ReadLine();
+Console.WriteLine("Insira a idade: ");
+pessoa.idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insira o CPF: ");
+pessoa.cpf = Console.ReadLine();
 
-//parte do Aluno
-Console.WriteLine("");
-Aluno AL = new Aluno();
-Console.WriteLine("Insira o RM");
-AL.RM = Console.ReadLine();
-Console.WriteLine("Insira o curso do aluno");
-AL.Curso = Console.ReadLine();
+if (escolaridade == "Professor" || escolaridade == "Professor")
+{
+    Professor prof = new Professor();
+    Console.WriteLine("Insira o nome");
+    prof.nome = Console.ReadLine();
+    Console.WriteLine("Insira a idade");
+    prof.idade = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Insira o CPF");
+    prof.cpf = Console.ReadLine();
+    prof.listarDados(prof.nome, prof.idade, prof.cpf);
+}
 
-AL.listarAluno(AL.RM, AL.Curso);
+else if (escolaridade == "Aluno" || escolaridade == "Aluno")
+{
+    Aluno AL = new Aluno();
+    Console.WriteLine("Insira o RM");
+    AL.RM = Console.ReadLine();
+    Console.WriteLine("Insira o curso do aluno");
+    AL.Curso = Console.ReadLine();
+
+    AL.listarAluno(AL.RM, AL.Curso);
+}
